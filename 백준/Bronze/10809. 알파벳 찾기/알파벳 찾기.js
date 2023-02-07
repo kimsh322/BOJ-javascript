bb0c6de646ef86e38eea931dd150930dbfb83a4f
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-07 09:51:03 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 156 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -1,5 +1,7 @@
 const a = require('fs').readFileSync('/dev/stdin').toString().trim();
 const alp = 'abcdefghijklmnopqrstuvwxyz';
+let arr=[]
 for(let el of alp){
-    process.stdout.write(a.indexOf(el) + " ");
+    arr.push(a.indexOf(el));
 };
+console.log(arr.join(' '))
