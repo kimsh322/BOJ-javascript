132aebe1042982050279e8db84674a25fb2b957f
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-11 11:52:43 +0900

    [Bronze II] Title: 손익분기점, Time: 120 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.js
+++ 백준/Bronze/1712. 손익분기점/손익분기점.js
@@ -1,12 +1,9 @@
 const a = require('fs').readFileSync('/dev/stdin').toString().split(" ");
 let result = 0;
 let sum = +a[0];
-let b = +a[1] - (+a[2]);
-if (b>=0) console.log(-1);
+let b = +a[1] - +a[2];
+if (b >= 0) console.log(-1);
 else {
-  while (sum >= 0) {
-    sum += b;
-    result++;
-  }
+  result = Math.floor(sum / -b) + 1;
   console.log(result);
 }
