e9fdc4086d0d8ef0aa66f4c059cf87c96ff3b95d
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-15 16:58:21 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 184 ms, Memory: 9856 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
@@ -1,10 +1,13 @@
 const a = require('fs').readFileSync('/dev/stdin').toString().split("\n");
+const arr = [];
+function sol(x, y) {
+  if (x === 0) return y;
+  if (y === 1) return 1;
+  if (arr[x] === undefined) arr[x] = [];
+  if (arr[x][y] === undefined) arr[x][y] = sol(x - 1, y) + sol(x, y - 1);
+  return arr[x][y];
+}
 for (let i = 1; i <= +a[0] * 2; i += 2) {
   let [k, n] = [+a[i], +a[i + 1]];
-  function sol(x, y) {
-    if (x === 0) return y;
-    if (y === 1) return 1;
-    return sol(x - 1, y) + sol(x, y - 1);
-  }
   console.log(sol(k, n));
 }
