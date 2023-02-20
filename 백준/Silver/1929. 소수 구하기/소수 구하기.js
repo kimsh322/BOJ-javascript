6f1c65fa51b075bf91a852f8cbb4fb21a820f3cd
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-20 09:54:10 +0900

    [Silver III] Title: 소수 구하기, Time: 400 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.js
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.js
@@ -1,6 +1,7 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split(" ").map((el) => +el);
 if (input[0] <= 2 && input[1] >= 2) console.log(2);
 if (input[0] % 2 === 0) input[0]++;
+let result = ''
 for (let i = input[0]; i <= input[1]; i += 2) {
   if (i < 3) continue;
   let isPrime = true;
@@ -10,5 +11,6 @@ for (let i = input[0]; i <= input[1]; i += 2) {
       break;
     }
   }
-  if (isPrime) console.log(i);
+  if (isPrime) result += i + '\n'
 }
+console.log(result);
