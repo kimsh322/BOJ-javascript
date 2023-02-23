95aec4b78f1f8af8b49a10d7898c703e5abaf084
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-23 15:33:52 +0900

    [Silver IV] Title: GCD 합, Time: 140 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.js
@@ -0,0 +1,15 @@
+const [num, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const gcd = (a, b) => {
+  if (a % b === 0) return b;
+  return gcd(b, a % b);
+};
+for (let i = 0; i < num; i++) {
+  let sum = 0;
+  let [a, ...arr] = input[i].split(" ").map(Number);
+  for (let j = 0; j < a - 1; j++) {
+    for (let k = j + 1; k < a; k++) {
+      sum += gcd(arr[j], arr[k]);
+    }
+  }
+  console.log(sum);
+}
