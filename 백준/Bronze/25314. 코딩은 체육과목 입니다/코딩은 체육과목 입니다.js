9f6a946fcf9090640e6c5e324d20231022462cf8
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-02-23 22:39:55 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,3 +1,4 @@
 const num = +require("fs").readFileSync('/dev/stdin').toString().trim();
-let result = "long ".repeat(Math.floor(num / 4)) + "int";
+let a = 'long '
+let result = a.repeat(Math.floor(num / 4)) + "int";
 console.log(result);
