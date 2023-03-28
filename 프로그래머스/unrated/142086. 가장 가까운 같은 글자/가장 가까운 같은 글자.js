8dcf741f9f6da495fdedf57d097487e1c4c4e965
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-03-28 21:22:32 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.13 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,13 +2,9 @@ function solution(s) {
     let obj = {};
     let result = [];
     for(let i=0; i<s.length; i++){
-        if(s[i] in obj) {
-            result.push(i-obj[s[i]]);
-            obj[s[i]] = i;
-        } else {
-            result.push(-1);
-            obj[s[i]] = i;
-        }
+        if(s[i] in obj) result.push(i-obj[s[i]]);
+        else result.push(-1);
+        obj[s[i]] = i;
     }
     return result;
 }
