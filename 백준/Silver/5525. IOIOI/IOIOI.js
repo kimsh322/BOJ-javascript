4879d3a720bb4ccc4ae62264753e59e683f990e3
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-03-29 09:41:26 +0900

    [Silver I] Title: IOIOI, Time: 176 ms, Memory: 9664 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+let p = "I" + "OI".repeat(+input[0]);
+let num = 0;
+for (let i = 0; i <= input[1] - p.length; i++) {
+  let bool = true;
+  for (let j = 0; j < p.length; j++) {
+    if (p[j] !== input[2][i + j]) {
+      bool = false;
+      break;
+    }
+  }
+  if (bool) num++;
+}
+console.log(num);
