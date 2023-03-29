bb70ff530e1cc0fb3a9f89e2ded5c7e96b698052
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-03-29 20:32:53 +0900

    [Silver I] Title: 경로 찾기, Time: 284 ms, Memory: 10784 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.js
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -2,27 +2,18 @@ const [n, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim()
 let arr = [];
 for (let i = 0; i < n; i++) arr.push(input[i].split(" ").map(Number));
 let result = Array(+n).fill(0).map(() => Array(+n).fill(0));
-let bool = false;
-const dfs = (array, x, y) => {
-  if (array[x][y] === 1) {
-      bool = true;
-      return;
-  }
-  array[x].forEach((el, idx) => {
-      if(bool) return;
-    if (el === 1) {
-      array[x][idx] = 0;
-      dfs(array, idx, y);
+const dfs = (cur, start, visited) => {
+  for (let i = 0; i < n; i++) {
+    if (arr[cur][i] === 1 && !visited[i]) {
+      visited[i] = 1;
+      result[start][i] = 1;
+      dfs(i, start, visited);
     }
-  });
+  }
 };
 
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    let arr2 = JSON.parse(JSON.stringify(arr));
-    bool = false;
-    dfs(arr2, i, j);
-    if (bool) result[i][j] = 1;
-  }
+  let visited = Array(+n).fill(0);
+  dfs(i, i, visited);
 }
 for (let i = 0; i < n; i++) console.log(result[i].join(" "));
