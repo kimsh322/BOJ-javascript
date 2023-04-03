d1427b43822aeb98d928474ffa22da7bade51ba4
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-04-03 11:18:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 560 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,24 +1,30 @@
 const [n, k] = require("fs").readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
-let queue = [[n, 0]];
-let result = 0;
+let queue = [n];
+let result = -1;
 let visited = {};
-while (queue.length) {
-  let [cur, num] = queue.shift();
-  if (cur === k) {
-    result = num;
-    break;
+let num = 0;
+while (true) {
+  let size = queue.length;
+  for (let i = 0; i < size; i++) {
+    let cur = queue.shift();
+    if (cur === k) {
+      result = num;
+      break;
+    }
+    if (!visited[cur + 1] && cur < 100000) {
+      queue.push(cur + 1);
+      visited[cur + 1] = true;
+    }
+    if (!visited[cur - 1] && cur > 0) {
+      queue.push(cur - 1);
+      visited[cur - 1] = true;
+    }
+    if (!visited[cur * 2] && cur * 2 <= 100000) {
+      queue.push(cur * 2);
+      visited[cur * 2] = true;
+    }
   }
-  if (!visited[cur + 1] && cur < 100000) {
-    queue.push([cur + 1, num + 1]);
-    visited[cur + 1] = true;
-  }
-  if (!visited[cur - 1] && cur > 0) {
-    queue.push([cur - 1, num + 1]);
-    visited[cur - 1] = true;
-  }
-  if (!visited[cur * 2] && cur * 2 <= 100000) {
-    queue.push([cur * 2, num + 1]);
-    visited[cur * 2] = true;
-  }
+  if (result >= 0) break;
+  num++;
 }
 console.log(result);
