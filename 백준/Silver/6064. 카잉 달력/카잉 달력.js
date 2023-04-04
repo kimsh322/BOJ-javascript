4c8dcfa96b1245975ccce08a0012df93e1bc1128
kimsh322 <118786973+kimsh322@users.noreply.github.com>
2023-04-04 10:24:54 +0900

    [Silver I] Title: 카잉 달력, Time: 420 ms, Memory: 13164 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.js
@@ -0,0 +1,21 @@
+const [t, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const gcd = (a, b) => {
+  if (a % b === 0) return b;
+  return gcd(b, a % b);
+};
+
+for (let i = 0; i < t; i++) {
+  let [m, n, x, y] = input[i].split(" ").map(Number);
+  let num = x;
+  let curY = x;
+  let lcm = (n * m) / gcd(n, m);
+  while (num <= lcm) {
+    let curYY = curY % n === 0 ? n : curY % n;
+    if (curYY === y) break;
+    curY = curYY + m;
+    num += m;
+  }
+  if (num > lcm) console.log(-1);
+  else console.log(num);
+}
