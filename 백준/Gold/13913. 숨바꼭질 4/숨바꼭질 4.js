29599e15ec03f528fcd1dcf32abb4c9dd282efe8
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-05-11 13:49:10 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 528 ms, Memory: 32072 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.js
@@ -1,26 +1,40 @@
 const input = require("fs").readFileSync('/dev/stdin').toString().trim();
 const [n,k] = input.split(' ').map(Number);
 let visited = Array(100001).fill(false);
-let queue = [[n,''+n]];
+let prev = Array(100001).fill(-2);
+prev[n] = -1;
+let queue = [n];
 let result = '';
+let number = -1;
 while(true){
-  let [cur,curStr] = queue.shift();
+  let cur = queue.shift();
   if(cur===k) {
-    result = curStr;
+    let resultArr = [];
+    const func = (num) => {
+      number++;
+      resultArr.push(num);
+      if(prev[num] === -1) return;
+      else func(prev[num]);
+    }
+    func(cur);
+    result = resultArr.reverse().join(' ');
     break;
   }
-  if(cur<100000 && !visited[cur+1]){
-    queue.push([cur+1,curStr+` ${cur+1}`]);
+  if(cur<100000 && !visited[cur+1] && prev[cur+1] ===-2){
+    queue.push(cur+1);
     visited[cur+1] = true;
+    prev[cur+1] = cur;
   }
-  if( 0<cur && !visited[cur-1]){
-    queue.push([cur-1,curStr+` ${cur-1}`]);
+  if( 0<cur && !visited[cur-1] && prev[cur-1] ===-2){
+    queue.push(cur-1);
     visited[cur-1] = true;
+    prev[cur-1] = cur;
   }
-  if(cur<=50000 && !visited[cur*2]){
-    queue.push([cur*2,curStr+` ${cur*2}`]);
+  if(cur<=50000 && !visited[cur*2] && prev[cur*2] ===-2){
+    queue.push(cur*2);
     visited[cur*2] = true;
+    prev[cur*2] = cur;
   }
 }
-console.log(result.split(' ').length-1);
+console.log(number);
 console.log(result);
