92593d193dab8d8ed8aa3e484c0cded74a755292
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-05-13 11:53:05 +0900

    [Silver II] Title: 에디터, Time: 416 ms, Memory: 108280 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.js
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -1,11 +1,13 @@
 const [init, n, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n');
 let left = init.split('');
 let right = [];
+let str = '';
 for(let i=0; i<n; i++){
-  let cur = input[i].split(' ');
+  let cur = input[i];
+  if(cur.length ===3) str = cur[2];
   if(cur[0] === 'L' && left.length) right.push(left.pop());
   if(cur[0] === 'D' && right.length) left.push(right.pop());
   if(cur[0] === 'B' && left.length) left.pop();
-  if(cur[0] === 'P') left.push(cur[1]);
+  if(cur[0] === 'P') left.push(str);
 }
 console.log(left.join('')+right.reverse().join(''));
