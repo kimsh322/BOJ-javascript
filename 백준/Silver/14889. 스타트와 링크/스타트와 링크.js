d5a54cb790f23afbb5972117a30ef6ce94547fc7
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-05-14 18:07:49 +0900

    [Silver II] Title: 스타트와 링크, Time: 1108 ms, Memory: 51108 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
@@ -0,0 +1,41 @@
+const [n, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n');
+let table = [];
+for(let i=0; i<n; i++) {
+  table.push(input[i].split(' ').map(Number));
+}
+let team=[];
+let newArr=[];
+let visited= Array(n).fill(false);
+const func = (num) =>{
+    if(num===n/2) {
+        team.push([...newArr]);
+        return;
+    }
+    for(let i=num; i<n; i++){
+        if(visited[i]) continue;
+        for(let j=0; j<=i ; j++) visited[j] = true;
+        newArr.push(i);
+        func(num+1);
+        newArr.pop(i);
+        for(let j=0; j<=i ; j++) visited[j] = false;
+    }
+}
+func(0);
+
+const func2 = (arr) => {
+    let sum=0;
+    for(let el of arr){
+        for(let i=0; i<n; i++){
+            if(arr.includes(i)) sum += table[el][i];
+        }
+    }
+    return sum;
+}
+
+let sum = team.map(el => func2(el));
+let leng = sum.length/2;
+let min = 10000;
+for(let i=0; i<leng; i++){
+    min = Math.min(min, Math.abs(sum[i] - sum[sum.length-i-1]));
+}
+console.log(min);
