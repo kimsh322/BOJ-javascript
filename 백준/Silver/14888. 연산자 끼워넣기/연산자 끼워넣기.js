7b4ee3cdd53d5972d62dab8a9516754c759bd679
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-05-15 13:44:06 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 212 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,47 +1,33 @@
 const [n,a,o] = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n');
 let arrA = a.split(' ').map(Number);
-let temp = o.split(' ').map(Number);
-let operatorSet = '';
-for(let j=0; j<temp.length; j++){
-    for(let i=0; i<temp[j]; i++) operatorSet += j;
-}
-let arr = new Set();
-let newArr = [];
-let visited = Array(operatorSet.length).fill(false);
-const func = (num) => {
-    if(num>=n-1) {
-        arr.add(newArr.join(''));
-        return;
-    }
-    for(let i=0; i<n-1 ;i++){
-        if(visited[i]) continue;
-        visited[i] = true;
-        newArr.push(operatorSet[i]);
-        func(num+1);
-        newArr.pop();
-        visited[i] = false;
-    }
-}
-func(0);
+let operatorSet = o.split(' ').map(Number);
 
-const operatorFunc = (a,b,str) => {
-    if(str === '0') return a+b;
-    if(str === '1') return a-b;
-    if(str === '2') return a*b;
-    if(str === '3') {
+const operatorFunc = (a,b,operator) => {
+    if(operator === 0) return a+b;
+    if(operator === 1) return a-b;
+    if(operator === 2) return a*b;
+    if(operator === 3) {
         let divide = Math.floor(Math.abs(a)/b);
         return a<0 ? -divide : divide;
     }
 }
 let result = []
-for(let curOperator of arr){
-    let a = arrA[0]
-    for(let i=1; i<n; i++){
-        let b = arrA[i];
-        a = operatorFunc(a,b,curOperator[i-1]);
+
+const func = (num,sum,curOperatorSet) => {
+    if(num===n-1) {
+        result.push(sum);
+        return;
     }
-    result.push(a);
+    for(let i=0; i<4; i++){
+        if(curOperatorSet[i]>0) {
+            curOperatorSet[i]--;
+            func(num+1,operatorFunc(sum,arrA[num+1],i),curOperatorSet);
+            curOperatorSet[i]++;
+        }
+    }
 }
+func(0,arrA[0],operatorSet);
+
 let max = Math.max(...result);
 let min = Math.min(...result);
 console.log(max === 0 ? 0 : max);
