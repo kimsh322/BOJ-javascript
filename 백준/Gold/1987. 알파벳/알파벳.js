238b311538cfe8b98e35524449a02150357f4f36
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-06-11 16:52:38 +0900

    [Gold IV] Title: 알파벳, Time: 2084 ms, Memory: 12540 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -4,22 +4,22 @@ const table = [];
 for (let i = 0; i < input.length; i++) {
   table.push(input[i].split(""));
 }
-const set = new Set();
-set.add(table[0][0]);
+const visitedAl = Array(26).fill(false);
+visitedAl[table[0][0].charCodeAt(0) - 65] = true;
 let max = 1;
 const dx = [0, 0, 1, -1];
 const dy = [1, -1, 0, 0];
-const func = (x, y) => {
-  max = Math.max(max, set.size);
+const func = (x, y, leng) => {
+  max = Math.max(max, leng);
   for (let i = 0; i < 4; i++) {
     let curX = x + dx[i];
     let curY = y + dy[i];
     if (curX < 0 || curX >= c || curY < 0 || curY >= r) continue;
-    if (set.has(table[curY][curX])) continue;
-    set.add(table[curY][curX]);
-    func(curX, curY);
-    set.delete(table[curY][curX]);
+    if (visitedAl[table[curY][curX].charCodeAt(0) - 65]) continue;
+    visitedAl[table[curY][curX].charCodeAt(0) - 65] = true;
+    func(curX, curY, leng + 1);
+    visitedAl[table[curY][curX].charCodeAt(0) - 65] = false;
   }
 };
-func(0, 0);
+func(0, 0, 1);
 console.log(max);
