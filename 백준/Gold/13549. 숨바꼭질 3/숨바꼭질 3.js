c6edeee755ddd547ca2085067b89def88012f59f
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-07-03 11:00:46 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 300 ms, Memory: 13448 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -1,15 +1,24 @@
 const [n, k] = require("fs").readFileSync('/dev/stdin').toString().trim().split(" ").map(Number);
-const dequeue = [[n, 0]];
-const visited = Array(100001).fill(false);
-visited[n] = true;
-while (dequeue.length) {
-  const [cur, curTime] = dequeue.shift();
-  visited[cur] = true;
+const queue = [n];
+const time = Array(100001).fill(Number.MAX_SAFE_INTEGER);
+const max = Number.MAX_SAFE_INTEGER;
+time[n] = 0;
+while (queue.length) {
+  const cur = queue.shift();
   if (cur === k) {
-    console.log(curTime);
+    console.log(time[cur]);
     break;
   }
-  if (cur * 2 <= 100000 && !visited[cur * 2]) dequeue.unshift([cur * 2, curTime]);
-  if (cur - 1 >= 0 && !visited[cur - 1]) dequeue.push([cur - 1, curTime + 1]);
-  if (cur + 1 <= 100000 && !visited[cur + 1]) dequeue.push([cur + 1, curTime + 1]);
+  if (cur * 2 <= 100000 && time[cur * 2] === max) {
+    time[cur * 2] = time[cur];
+    queue.unshift(cur * 2);
+  }
+  if (cur - 1 >= 0 && time[cur - 1] === max) {
+    time[cur - 1] = time[cur] + 1;
+    queue.push(cur - 1);
+  }
+  if (cur + 1 <= 100000 && time[cur + 1] === max) {
+    time[cur + 1] = time[cur] + 1;
+    queue.push(cur + 1);
+  }
 }
