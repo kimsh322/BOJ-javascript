12ba7d2c447594a14a1f5cfb1e3f5327f11bbcb5
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-07-12 12:28:51 +0900

    [Gold III] Title: 나머지 합, Time: 580 ms, Memory: 128108 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.js
+++ 백준/Gold/10986. 나머지 합/나머지 합.js
@@ -2,15 +2,14 @@ const [a, input] = require("fs").readFileSync('/dev/stdin').toString().trim().sp
 const [n, m] = a.split(" ").map(Number);
 const arr = input.split(" ").map(Number);
 const modArr = Array(m).fill(0);
-modArr[0] = 1;
 let sum = 0;
-let count = 0;
 
 for (let i = 0; i < n; i++) {
   let cur = arr[i] % m;
   sum = (sum + cur) % m;
   modArr[sum] += 1;
 }
+let count = modArr[0];
 
 for (let i = 0; i < m; i++) {
   count += (modArr[i] * (modArr[i] - 1)) / 2;
