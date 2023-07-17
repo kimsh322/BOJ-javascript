41bd98aa39df95aa0efe28d79890cefd5075a403
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-07-17 11:30:43 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 340 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.js
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.js
@@ -9,6 +9,26 @@ const dist = (a, b) => {
 };
 
 const newArr = [];
+let result = Number.MAX_SAFE_INTEGER;
+
+const combi = (arr, visited) => {
+  if (newArr.length === 3) {
+    result = Math.min(
+      result,
+      dist(newArr[0], newArr[1]) + dist(newArr[1], newArr[2]) + dist(newArr[2], newArr[0])
+    );
+    return;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    newArr.push(arr[i]);
+    combi(arr, visited);
+    newArr.pop();
+    visited[i] = false;
+  }
+};
+
 let str = "";
 
 for (let i = 0; i < 2 * t; i += 2) {
@@ -19,27 +39,9 @@ for (let i = 0; i < 2 * t; i += 2) {
   }
   const arr = input[i + 1].split(" ");
   const visited = Array(n).fill(false);
-  let result = Number.MAX_SAFE_INTEGER;
+  result = Number.MAX_SAFE_INTEGER;
 
-  const combi = () => {
-    if (newArr.length === 3) {
-      result = Math.min(
-        result,
-        dist(newArr[0], newArr[1]) + dist(newArr[1], newArr[2]) + dist(newArr[2], newArr[0])
-      );
-      return;
-    }
-    for (let i = 0; i < n; i++) {
-      if (visited[i]) continue;
-      visited[i] = true;
-      newArr.push(arr[i]);
-      combi();
-      newArr.pop();
-      visited[i] = false;
-    }
-  };
-
-  combi(arr, visited, result);
+  combi(arr, visited);
   str += result + "\n";
 }
 
