ae976f4c27beb56fb88d6647a86c6b1ad5193c59
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-07-25 11:16:47 +0900

    [Gold IV] Title: 사이클 게임, Time: 1844 ms, Memory: 282932 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.js
@@ -0,0 +1,33 @@
+const [a, ...input] = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n");
+const [n, m] = a.split(" ").map(Number);
+const arr = [];
+for (let i = 0; i < m; i++) {
+  arr.push(input[i].split(" ").map(Number));
+}
+
+const parent = [];
+for (let i = 0; i < n; i++) parent.push(i);
+
+const findRoot = (x) => {
+  if (parent[x] === x) return x;
+  return (parent[x] = findRoot(parent[x]));
+};
+
+const compareRoot = (a, b) => {
+  let x = findRoot(a);
+  let y = findRoot(b);
+  if (x === y) return true;
+  else {
+    parent[x] = y;
+    return false;
+  }
+};
+let result = 0;
+for (let i = 0; i < m; i++) {
+  let [a, b] = arr[i];
+  if (compareRoot(a, b)) {
+    result = i + 1;
+    break;
+  }
+}
+console.log(result);
