34b4f09f34c7f0d6ad3e0d13047ce6f3e0e8efc7
SuHyeon Kim <118786973+kimsh322@users.noreply.github.com>
2023-09-30 20:44:56 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 7.73 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,73 @@
+function solution(orders, course) {
+    const isSatisCourse = (a) => {
+        let count=0;
+        for(let order of orders) {
+            if(a.length > order.length) continue;
+            let isTrue = true;
+            for(let i=0; i<a.length; i++) {
+                if(!isTrue) break;
+                if(!order.includes(a[i])) isTrue = false;
+            }
+            if(isTrue) count++;
+        }
+        return count ;
+    }
+    
+    // 사용하는 알파벳 구하기
+    const checkAllAlpha = new Set();
+    for(let order of orders) {
+        for(let i=0; i<order.length; i++) {
+            checkAllAlpha.add(order[i]);
+        }
+    }
+    const leng = checkAllAlpha.size
+    const arr = Array.from(checkAllAlpha);
+    arr.sort();
+    
+    let allCourse = new Set();
+    const tempArr = [];
+    const visited = Array(leng).fill(false);
+    
+    // 가능한 모든 course 구하기
+    const getAllCourse = (a,index,order) => {
+        if(tempArr.length >= a) {
+            allCourse.add(tempArr.join(''));
+            return;
+        }
+        
+        for(let i=index; i<order.length ;i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            tempArr.push(order[i]);
+            getAllCourse(a,i,order);
+            tempArr.pop();
+            visited[i] = false;
+        }
+    }
+    
+    const result = [];
+
+    for(let number of course) {
+        allCourse = new Set();
+        for(let order of orders) {
+            order = order.split('').sort().join('');
+            getAllCourse(number,0,order);
+        }
+
+        let countArr = [];
+        for(let course of allCourse) {
+            let count = isSatisCourse(course);
+            countArr.push([course,count]);
+        }
+        if(countArr.length === 0) continue;
+        countArr.sort((a,b) => b[1] - a[1]);
+        const max = countArr[0][1];
+        if(max < 2) continue;
+        for(let i=0; i<countArr.length; i++) {
+            if(countArr[i][1] < max) break;
+            result.push(countArr[i][0]);
+        }
+    }
+    return result.sort();
+    
+}
